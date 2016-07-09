db7d5880714246adb5d601a04f7337b0a4610a65
Alex <alexxnavarretee@gmail.com>
2016-07-09 12:23:46 -0600

    test setup

---
+++ test/index.js
@@ -0,0 +1,1 @@
+require('./user-movement-test.js')
---
+++ test/user-movement-test.js
@@ -0,0 +1,13 @@
+const chai      = require('chai').assert;
+const GameBoard = require('../lib/gameBoard.js');
+
+
+var expect = require('chai').expect;
+
+describe('GameBoard', function() {
+  context('test', function(){
+    it('test', function(){
+      expect('bar').to.equal('bar');
+    });
+  });
+});
